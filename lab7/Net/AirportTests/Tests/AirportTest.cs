681b614004263da0ff4768abebf113ec960b1011
Ilya Tovpeko <mcilyasat@mail.ru>
2021-11-04 22:36:27 +0300

    NUnitTests

--- lab7/Net/AirportTests/Tests/AirportTest.cs
+++ lab7/Net/AirportTests/Tests/AirportTest.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace XUnitTests.Tests
+namespace AirportTest.Tests
 {
     [TestFixture]
     public class AirportTest
--- lab7/Net/AirportTests/obj/Debug/netcoreapp3.1/AirportTests.AssemblyInfo.cs
+++ lab7/Net/AirportTests/obj/Debug/netcoreapp3.1/AirportTests.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("XUnitTests")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AirportTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("XUnitTests")]
-[assembly: System.Reflection.AssemblyTitleAttribute("XUnitTests")]
+[assembly: System.Reflection.AssemblyProductAttribute("AirportTests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AirportTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
