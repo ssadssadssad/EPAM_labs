9e3b0267ee0fd5bff9705b55561c170af8edceeb
Ilya Tovpeko <mcilyasat@mail.ru>
2021-11-04 20:13:35 +0300

    MSTest

---
+++ lab7/Net/MSTest/Tests/AirportTest.cs
@@ -0,0 +1,56 @@
+﻿using Aircompany;
+using Aircompany.Models;
+using Aircompany.Planes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSTest.Tests
+{
+    [TestFixture]
+    public class AirportTest
+    {
+        private List<Plane> planes = new List<Plane>(){
+           new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
+           new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
+           new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
+           new PassengerPlane("Airbus A320", 930, 11800, 65500, 188),
+           new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
+           new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
+           new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
+           new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
+           new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.Bomber),
+           new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.Bomber),
+           new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.Bomber),
+           new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.Fighter),
+           new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.Fighter),
+           new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.Transport)
+        };
+
+        private PassengerPlane planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
+
+
+        [Test]
+        public void GetTransportMilitaryPlanesTest()
+        {
+            Airport airport = new Airport(planes);
+            Assert.True(airport.GetTransportMilitaryPlanes().Where(plane => plane.militaryType == MilitaryType.Transport).Any());
+        }
+
+        [Test]
+        public void GetPassengerPlaneWithMaxPassengersCapacityTest()
+        {
+            Airport airport = new Airport(planes);
+            Assert.AreEqual(planeWithMaxPassengerCapacity, airport.GetPassengerPlaneWithMaxPassengersCapacity());
+        }
+
+        [Test]
+        public void SortByMaxLoadCapacityTest()
+        {
+            Airport airport = new Airport(planes);
+            CollectionAssert.AreEqual(airport.Planes.OrderBy(plane => plane.maxLoadCapacity), airport.SortByMaxLoadCapacity().Planes);
+        }
+    }
+}
