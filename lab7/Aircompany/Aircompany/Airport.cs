5249e4a19cf025723627db153c6c189dd8ec6a15
Ilya Tovpeko <mcilyasat@mail.ru>
2021-11-07 20:21:22 +0300

    deleted unused libraries

--- lab7/Aircompany/Aircompany/Airport.cs
+++ lab7/Aircompany/Aircompany/Airport.cs
@@ -1,6 +1,5 @@
 ﻿using Aircompany.Models;
 using Aircompany.Planes;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
--- lab7/Aircompany/AircompanyTests/Tests/AircompanyTests.cs
+++ lab7/Aircompany/AircompanyTests/Tests/AircompanyTests.cs
@@ -2,10 +2,8 @@
 using Aircompany.Models;
 using Aircompany.Planes;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AircompanyTest.Tests
 {
